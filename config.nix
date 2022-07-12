41abf8c71e00b10db51de9b2a7f029c34207a305
Zachry Thayer <zthayer@anduril.com>
2022-07-12 11:36:07 -0700

    initial

---
+++ config.nix
@@ -0,0 +1,100 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [];
+
+  time.timeZone = "America/Los_Angeles";
+
+  networking.useDHCP = true;
+  services.xserver.enable = true;
+
+  services.xserver.desktopManager = {
+     xterm.enable = false;
+  };
+  services.xserver.displayManager.defaultSession = "none+i3";
+  services.xserver.windowManager.i3 = {
+    enable = true;
+    extraPackages = with pkgs; [ rofi dmenu i3status i3lock i3blocks i3lock-fancy ];
+  };
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.touchpad.naturalScrolling = true;
+  services.openssh.enable = true;
+
+  programs.ssh.startAgent = true;
+  services.avahi = {
+  	enable = true;
+	publish = {
+		enable = true;
+		addresses = true;
+		workstation = true;
+	};
+  };
+  services.globalprotect = {
+    enable = true;
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  programs.fish.enable = true;
+  users.users.zthayer = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "dialout" ];
+    shell = pkgs.fish;
+  };
+
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    (vscode-with-extensions.override { vscodeExtensions = with vscode-extensions; [
+      vadimcn.vscode-lldb
+      matklad.rust-analyzer
+      arrterian.nix-env-selector
+      bbenoist.nix
+      brettm12345.nixfmt-vscode
+      zhuangtongfa.material-theme
+      pkief.material-icon-theme
+      bungcip.better-toml
+      serayuzgur.crates
+      usernamehw.errorlens
+      eamodio.gitlens
+    ];})
+    unzip
+    rustup
+    lldb
+    zip
+    wget
+    git
+    fzf
+    bat
+    alacritty
+    firefox
+    usbutils
+    can-utils
+    xorg.xrandr
+    flameshot
+    globalprotect-openconnect
+    i3status-rust
+  ];
+
+  fonts.fonts = with pkgs; [
+    font-awesome
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+  ];
+
+
+  nixpkgs.config.allowUnfree = true;
+  nix.binaryCaches = [
+    "https://cache.nixos.org/"
+    "https://s3-us-west-2.amazonaws.com/anduril-nix-cache"
+    "https://s3-us-west-2.amazonaws.com/anduril-nix-polyrepo-cache"
+  ];
+  nix.binaryCachePublicKeys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "anduril-nix-cache:0FYOuMqEzbSX2PmByfePpJAsSV6CW+1YWoq7b21NxHc="
+    "anduril-nix-polyrepo-cache:0FYOuMqEzbSX2PmByfePpJAsSV6CW+1YWoq7b21NxHc="
+  ];
+
+
+  system.stateVersion = "22.05";
+}
