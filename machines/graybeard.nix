6e0e6fd87a64659e7190e686536b7e39cdc96819
Zachry Thayer <zachthayer@gmail.com>
2026-02-04 10:21:08 -0800

    org

---
+++ machines/graybeard.nix
@@ -0,0 +1,19 @@
+{...}: {
+  imports = [
+    ../hardware/graybeard.nix
+    ../user.nix
+    ../wireless.nix
+    ../cooler.nix
+    ../ui.nix
+    ../home.nix
+    ../ci.nix
+    # Users
+    (import ../lib/mkUser.nix (import ../users/zach.nix))
+  ];
+
+  networking.firewall.allowedUDPPorts = [63436 8000];
+  nixpkgs.config.allowUnfree = true;
+  networking.hostName = "graybeard";
+
+  services.ddns-updater.enable = true;
+}
